b681e3c263713db31c8a7983bf2e727f8886d9ca
Cierra_Runis <2864283875@qq.com>
2024-04-19 22:40:31 +0800

    📖Add more components

---
+++ app/src/app/components/NavBar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  Link,
+  Badge,
+} from "@nextui-org/react";
+import { ThemeChanger } from "../ThemeChanger";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
+import NextLink from "next/link";
+
+export default function NavBar() {
+  const pathname = usePathname();
+
+  return (
+    <Navbar shouldHideOnScroll isBordered isBlurred>
+      <NavbarBrand>
+        <Link
+          href='/'
+          as={NextLink}
+          className='flex gap-2 items-center'
+          color='foreground'
+        >
+          <p className='text-2xl'>🎨</p>
+          <p className='text-xl font-bold'>InPaint</p>
+        </Link>
+      </NavbarBrand>
+
+      <NavbarContent justify='end'>
+        <NavbarItem>
+          <Link href='/result' as={NextLink} isBlock>
+            我的文件
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <ThemeChanger />
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
---
+++ app/src/app/provider.tsx
@@ -0,0 +1,12 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider className='text-foreground bg-background'>
+      <NextThemesProvider attribute='class' defaultTheme='dark'>
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
+}
