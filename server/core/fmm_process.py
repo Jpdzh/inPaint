037ba2d5c01f29304a290251e2bd53f561322c42
Jpdzh <h3067999590@outlook.com>
2024-04-25 20:31:45 +0800

    modify two process file for returned value

--- server/core/fmm_process.py
+++ server/core/fmm_process.py
@@ -4,45 +4,48 @@ from PIL import Image
 import json
 from server.core.FMM_ImageInpainter import ImageInpainter
 import cv2
-import numpy as np
+from server.core.utils import base64_to_cv2, numpy_to_base64
+from server.core.evaluation import calc_psnr, calc_ssim, calc_lpips
 
 
-def numpy_to_base64(image_np):
-    data = cv2.imencode('.jpg', image_np)[1]
-    image_bytes = data.tobytes()
-    image_base4 = base64.b64encode(image_bytes).decode('utf8')
-    return image_base4
+def ffm_process(img_base64: str, masked_base64: str):
 
-def base64_to_cv2(base64_str, is_grey):
-    # 解码 Base64 字符串为图像数据
-    image_data = base64.b64decode(base64_str)
+    original_path = '../core/log/fmm_res/original.png'
+    inpainted_path = '../core/log/fmm_res/inpainted.png'
 
-    # 将图像数据转换为 NumPy 数组
-    nparr = np.frombuffer(image_data, np.uint8)
+    original_img = base64_to_cv2(img_base64, 0)
+    masked_img = base64_to_cv2(masked_base64, 0)
+    cv2.imwrite(original_path, original_img)
 
-    # 使用 OpenCV 读取图像
-    if is_grey:
-        img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-    else:
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    # 将两张图像转换为灰度图
+    original_gray = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
+    masked_gray = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY)
 
-    return img
+    #提取mask
+    mask = cv2.absdiff(original_gray, masked_gray)
+    _, mask = cv2.threshold(mask, 30, 255, cv2.THRESH_BINARY)
 
-def ffm_process(img_base64, mask_base64):
-    # 将 Base64 字符串转换为 OpenCV 图像
-    img = base64_to_cv2(img_base64, False)
-    mask = base64_to_cv2(mask_base64, True)
-
-    inpainter = ImageInpainter(img)
+    inpainter = ImageInpainter(original_img)
     inpainter.inpaint(mask, 5)
-    output=inpainter.getOutput()
-    ouput_base64=numpy_to_base64(output)
-    psnr=29.876
+    inpainted_img = inpainter.getOutput()
 
+
+    cv2.imwrite(inpainted_path, inpainted_img)
+
+    ssim_score = calc_ssim(original_path, inpainted_path)
+    psnr_score = calc_psnr(original_path, inpainted_path)
+    lpips_score = calc_lpips(original_path, inpainted_path)
+
+    inpainted_base64 = numpy_to_base64(inpainted_img)
 
     response_data={
-      'inpaint_img': ouput_base64,
-      'psnr': psnr
 
+        'model': 'FMM',
+        'orginal_img': img_base64,
+        'masked_img':masked_base64,
+        'inpaint_img': inpainted_base64,
+        'psnr': psnr_score,
+        'ssmi':ssim_score,
+        'lpips':lpips_score
     }
     return response_data
